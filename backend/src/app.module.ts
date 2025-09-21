021e25e4d8be08e1d25bd97d3feb5b77d580565b
gitlao93 <laojimmarklino@gmail.com>
2025-09-21 23:07:24 +0800

    Added Agency Module feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AgencyModule } from './agency/agency.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      database: 'goodlife_insurance_db',
+      entities: [],
+      synchronize: true, // ⚠️ disable in prod
+      logging: false,
+    }),
+    UserModule,
+    AgencyModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
