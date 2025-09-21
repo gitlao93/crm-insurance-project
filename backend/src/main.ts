63203e157c55b46451ff3516e98cfc808f17f1a4
gitlao93 <laojimmarklino@gmail.com>
2025-09-22 02:51:24 +0800

    Added the validation pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips out properties not in DTO
+      forbidNonWhitelisted: true, // throws error on extra fields
+      transform: true, // transforms payloads to DTO classes
+    }),
+  );
+
+  await app.listen(3001);
 }
 bootstrap();
